ce34f8fd219aae3b17f08fb6867c20c655870762
Ricky Iza <rck.ikbm@hotmail.com>
2021-01-20 11:12:37 -0500

    Add Project

---
+++ src/test/java/com/iza/backend/BackendIzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iza.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
